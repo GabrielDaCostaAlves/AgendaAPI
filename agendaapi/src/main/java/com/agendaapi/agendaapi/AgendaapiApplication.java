51bcdb1337052c1b727abd061e80ff0c39a627fc
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2025-01-04 10:05:48 -0300

    Add files via upload
    initial commit

---
+++ agendaapi/src/main/java/com/agendaapi/agendaapi/AgendaapiApplication.java
@@ -0,0 +1,13 @@
+package com.agendaapi.agendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaapiApplication.class, args);
+	}
+
+}
