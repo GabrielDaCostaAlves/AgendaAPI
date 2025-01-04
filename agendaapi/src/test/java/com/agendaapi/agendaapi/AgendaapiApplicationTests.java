e2c5325c3319c1420425ab609504f39fcbcb1b7d
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2025-01-04 10:05:48 -0300

    Add files via upload
    initial commit

---
+++ agendaapi/src/test/java/com/agendaapi/agendaapi/AgendaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendaapi.agendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
